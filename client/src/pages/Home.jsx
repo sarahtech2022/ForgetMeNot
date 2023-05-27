9306012f35ec062dcce896bf311878fdaca49cef
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-05-26 18:20:04 -0600

    adding custom avatar for cohort member

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import ListLoves from "../components/ListLoves";
+import Rahma from "../assets/Rahma.jpg";
 
 const Home = () => {
   const { loginWithRedirect } = useAuth0();
   return (
     <div>
       <ListLoves />
+      <img src={Rahma} width="200px"></img>
     </div>
   );
 };
